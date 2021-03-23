bfa754d9c5c38b847e761604a81a5d15a6668105
gary <gary.white@lodgit.com>
2021-03-23 17:10:15 +0100

    changes

---
+++ code.js
@@ -0,0 +1,7 @@
+var data = "name|phone|address";
+var val = data.substring(5, 10);
+console.log("Substring is " + val);
+
+var data = "name|phone|address";
+var vals = data.split("|");
+console.log("Split array is " , vals);
