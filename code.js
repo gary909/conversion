559b11256a1b003bc2436c7c07f8922d162da7c6
gary <gary.white@lodgit.com>
2021-03-23 17:23:13 +0100

    code

--- code.js
+++ code.js
@@ -2,6 +2,22 @@ var data = "name|phone|address";
 var val = data.substring(5, 10);
 console.log("Substring is " + val);
 
+//***********************/
+
 var data = "name|phone|address";
 var vals = data.split("|");
 console.log("Split array is " , vals);
+
+//***********************/
+
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function() {console.log(this.sound);}
+}
+
+var toy = new Duck("quack quack");
+
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
